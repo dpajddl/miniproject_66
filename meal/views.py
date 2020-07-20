cdf9ac424ab6341f957446e672255532dfab1977
dpajddl <970won@gmail.com>
2020-07-20 14:45:37 +0900

    sfsdf

--- meal/views.py
+++ meal/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import *
+from .models import Restaurant
 from random import *
 
 def index_login(request):
@@ -14,7 +14,9 @@ def search_all(request):
     return render(request, 'meal/search_all.html', {})
 
 def random_lunch(request):
-    return render(request, 'meal/random_lunch.html', {})
+    r_list = Restaurant.objects.all()
+
+    return render(request, 'meal/random_lunch.html', {"r_list" : r_list})
 
 def mylocation(request):
     return render(request, 'meal/mylocation.html', {})
