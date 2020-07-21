84d62b2a4f5a78ff2b8f6335e507c01ad88b194c
dpajddl <970won@gmail.com>
2020-07-21 16:22:07 +0900

    DB 수정 주소지 제대로

--- meal/views.py
+++ meal/views.py
@@ -19,40 +19,7 @@ def index(request):
     return render(request, 'meal/index.html', {})
 
 def search_all(request):
-  
-    url = "https://dapi.kakao.com/v2/local/search/keyword.json?"
-    apikey = "0fd8917caae3b9798b5233596bbdd2e7"
-    x = request.session['user']['uesr_loc_x']
-    y = request.session['user']['uesr_loc_y']
-    restaurant_all=[]
-    r = requests.get( url, params = {'query':'식당',
-            'category_group_code':'FD6',
-        'x':x,
-        'y':y,
-        'radius':300}, headers={'Authorization' : 'KakaoAK ' + apikey } )
-    obj=r.json()
-    print(obj)
-
-    counts=obj['meta']['pageable_count']
-    total_pages= counts//15 if counts%15 == 0 else counts //15+1
-    print(total_pages)
-
-    for page in range(total_pages):
-        print(page+1)
-        r = requests.get( url, params = {'query':'식당',
-            'category_group_code':'FD6',
-            'x': x,
-        'y':y,
-        'radius':300,'page':page+1}, headers={'Authorization' : 'KakaoAK ' + apikey } )
-        obj=r.json()
-        docs=obj['documents']
-        for doc in docs:
-            restaurant_all.append(doc)
-        
-
-
-    request.session['rest'] = restaurant_all
-    print(request.session['rest'])
+    restaurant_all = request.session['rest']
     return render(request, 'meal/search_all.html', {'restaurant_all' : restaurant_all})
 
 
@@ -90,6 +57,37 @@ def login_function(request) :
             if user_all[i].user_pw == login_pw:
                 obj = model_to_dict(user_all[i])
                 request.session['user'] = obj
+                url = "https://dapi.kakao.com/v2/local/search/keyword.json?"
+                apikey = "0fd8917caae3b9798b5233596bbdd2e7"
+                x = request.session['user']['user_loc_x']
+                y = request.session['user']['user_loc_y']
+                restaurant_all=[]
+                r = requests.get( url, params = {'query':'식당',
+                        'category_group_code':'FD6',
+                    'x':x,
+                    'y':y,
+                    'radius':300}, headers={'Authorization' : 'KakaoAK ' + apikey } )
+                obj=r.json()
+                print(obj)
+
+                counts=obj['meta']['pageable_count']
+                total_pages= counts//15 if counts%15 == 0 else counts //15+1
+                print(total_pages)
+
+                for page in range(total_pages):
+                    print(page+1)
+                    r = requests.get( url, params = {'query':'식당',
+                        'category_group_code':'FD6',
+                        'x': x,
+                    'y':y,
+                    'radius':300,'page':page+1}, headers={'Authorization' : 'KakaoAK ' + apikey } )
+                    obj=r.json()
+                    docs=obj['documents']
+                    for doc in docs:
+                        restaurant_all.append(doc)
+                        
+                request.session['rest'] = restaurant_all
+                print(request.session['rest'])
                 return HttpResponse('0')
             else :
                 return HttpResponse('1')
@@ -110,26 +108,25 @@ def signup_function(request) :
         if user_all[i].user_id == signup_id:
             return HttpResponse('1')
             s_up = s_up + 1
+    if s_up == 0:
         param = {'query': signup_ad}
         header = {'Authorization' : 'KakaoAK d4be7b479f4b4cbd99bd19ae87f88b4b'}
-
         req = requests.get('https://dapi.kakao.com/v2/local/search/address.json', params=param, headers=header)
         obj = req.json()
         docs = obj['documents']
-        #add=obj['address']
-        print(docs)
-        for doc in docs:
-            x=doc['address']['x']
-            y=doc['address']['y']
-            print(x,y)
-        user_loc_x = x
-        user_loc_y = y
-    if s_up == 0:
-
+        if docs == []:
+            return HttpResponse('2')
+        else :
+            for doc in docs:
+                xx=doc['address']['x']
+                yy=doc['address']['y']
+            user_loc_x = xx
+            user_loc_y = yy
+        
         newbie = User(
             user_id=signup_id, user_pw=signup_pw,
             user_email=signup_email, user_nick = signup_nick,
-            uesr_loc_x = user_loc_x, uesr_loc_y = user_loc_y)
+            user_loc_x = user_loc_x, user_loc_y = user_loc_y)
         newbie.save();
         return HttpResponse('0')
     
@@ -148,14 +145,19 @@ def mylocation_function(request) :
     req = requests.get('https://dapi.kakao.com/v2/local/search/address.json', params=param, headers=header)
     obj = req.json()
     docs = obj['documents']
-    for doc in docs:
-        xx=doc['address']['x']
-        yy=doc['address']['y']
-    my_id = request.session['user']['user_id']
-    my_user.uesr_loc_x = xx
-    my_user.uesr_loc_y = yy
-    my_user.save()
-    return HttpResponse('0')
+    if docs == []:
+        return HttpResponse('1')
+    else :
+        for doc in docs:
+            xx=doc['address']['x']
+            yy=doc['address']['y']
+            print(type(xx))
+            print(doc)
+            print(xx, yy)
+        my_user.user_loc_x = xx
+        my_user.user_loc_y = yy
+        my_user.save()
+        return HttpResponse('0')
 
 
 
